c77392be1ecb5a43b6367affde6c11ffc82ba5d3
Priyanshu Anand <anandpriyanshu6@gmail.com>
2023-05-09 12:24:52 +0530

    First Controller Method

---
+++ src/main/java/com/anshu/jenkinsdemo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.anshu.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/home")
+	public String getName()
+	{
+		return "PRIYANSHU";
+	}
+}
